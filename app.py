f244ed4a969aec01e072dfb00a15a30cbd5d3a5a
ChoiMyeongHun <92916376+naiana51@users.noreply.github.com>
2024-07-20 10:06:59 +0900

    app.py

--- app.py
+++ app.py
@@ -139,7 +139,7 @@ for i in range(0, len(st.session_state.pokemons), 3):
     for j in range(len(row_pokemons)):
         with cols[j]:
             pokemon = row_pokemons[j]
-            with st.expander(label=f"**{i+j+1}. {pokemon["name"]}**", expanded=True):
+            with st.expander(label=f"**{i+j+1}. {pokemon['name']}**", expanded=True):
                 # st.subheader(pokemon["name"])
                 st.image(pokemon["image_url"])
                 emoji_types = [f"{type_emoji_dict[x]} {x}"for x in pokemon["types"]]
